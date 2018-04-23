8059e1569ffd8db03e7470295d6da234307177dd
Paul Fasola <paul.fasola@gmail.com>
2018-04-24 01:05:16 +0200

    Re-seal lab 2 (last commit was lab 3)

--- Blockchain101+SmartContracts/Labs/Partial/2_TokenCreation/test/token.test.js
+++ Blockchain101+SmartContracts/Labs/Partial/2_TokenCreation/test/token.test.js
@@ -2,6 +2,8 @@
 // TODO : Replace the values below with yours ;)
 const tokenName = "";
 const initialSupply = 0;
+const amount = 0;
+//
 
 const token = artifacts.require("./" + tokenName + ".sol");
 
@@ -13,9 +15,16 @@ contract(tokenName, async (accounts) => {
     */
   })
 
-  it("should send 10 coins correctly from owner account (0) to another one.", async () => {
+  it(`should send ${amount} ${tokenName} correctly from owner account (0) to another one (without approval)`, async () => {
     /*
       TODO : we need this test!
     */
   });
+
+  it(`sould send ${amount} ${tokenName} from an account to another (with approval!)`, async () => {
+    /*
+      TODO : we need this test!
+    */
+  });
+
 });
